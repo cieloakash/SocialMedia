a05af95b9a7b4bfe5aa29900751a5ade1a537bf9
Sky <akpakash490@gmail.com>
2025-04-16 20:17:59 +0530

    rpc fn & added count and comment count in post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,8 +4,8 @@ import { supabase } from '../supabse-client'
 import PostItem from './PostItem'
 
 const fetchPosts = async()=>{
-  const {data,error} = await supabase.from("posts").select("*").order('created_at',{ascending:false})
-  // const {data,error} = await supabase.rpc("get_posts_with_counts")
+  // const {data,error} = await supabase.from("posts").select("*").order('created_at',{ascending:false})
+  const {data,error} = await supabase.rpc("get_posts_with_counts")
     if(error)throw new Error(error.message);
     return data
 }
@@ -17,7 +17,6 @@ const PostList = () => {
     const {data,error,isLoading} = useQuery({queryKey:["posts"],queryFn:fetchPosts})
     if(isLoading)return <div>Loading Post...</div>
     if(error) return <div>Error: {error.message}</div>
-    console.log(data);
   return (
     <div className="flex flex-wrap gap-6 justify-center">
     {data?.map((post, key) => (
